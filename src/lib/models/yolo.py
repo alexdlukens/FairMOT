b925a7d6f9f41e28a97bb83ccc718b0053db3596
Alex <alukens@plugout.com>
2024-07-05 13:22:29 -0400

    ruff formatted everything

--- src/lib/models/yolo.py
+++ src/lib/models/yolo.py
@@ -20,9 +20,13 @@ class Detect(nn.Module):
         self.na = len(anchors[0]) // 2  # number of anchors
         self.grid = [torch.zeros(1)] * self.nl  # init grid
         a = torch.tensor(anchors).float().view(self.nl, -1, 2)
-        self.register_buffer('anchors', a)  # shape(nl,na,2)
-        self.register_buffer('anchor_grid', a.clone().view(self.nl, 1, -1, 1, 1, 2))  # shape(nl,1,na,1,1,2)
-        self.m = nn.ModuleList(nn.Conv2d(x, self.no * self.na, 1) for x in ch)  # output conv
+        self.register_buffer("anchors", a)  # shape(nl,na,2)
+        self.register_buffer(
+            "anchor_grid", a.clone().view(self.nl, 1, -1, 1, 1, 2)
+        )  # shape(nl,1,na,1,1,2)
+        self.m = nn.ModuleList(
+            nn.Conv2d(x, self.no * self.na, 1) for x in ch
+        )  # output conv
         self.inplace = inplace  # use in-place ops (e.g. slice assignment)
 
     def forward(self, x):
@@ -31,7 +35,12 @@ class Detect(nn.Module):
         for i in range(self.nl):
             x[i] = self.m[i](x[i])  # conv
             bs, _, ny, nx = x[i].shape  # x(bs,255,20,20) to x(bs,3,20,20,85)
-            x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
+            x[i] = (
+                x[i]
+                .view(bs, self.na, self.no, ny, nx)
+                .permute(0, 1, 3, 4, 2)
+                .contiguous()
+            )
 
             if not self.training:  # inference
                 if self.grid[i].shape[2:4] != x[i].shape[2:4] or self.onnx_dynamic:
@@ -39,11 +48,15 @@ class Detect(nn.Module):
 
                 y = x[i].sigmoid()
                 if self.inplace:
-                    y[..., 0:2] = (y[..., 0:2] * 2. - 0.5 + self.grid[i]) * self.stride[i]  # xy
+                    y[..., 0:2] = (
+                        y[..., 0:2] * 2.0 - 0.5 + self.grid[i]
+                    ) * self.stride[i]  # xy
                     y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
                 else:  # for YOLOv5 on AWS Inferentia https://github.com/ultralytics/yolov5/pull/2953
-                    xy = (y[..., 0:2] * 2. - 0.5 + self.grid[i]) * self.stride[i]  # xy
-                    wh = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i].view(1, self.na, 1, 1, 2)  # wh
+                    xy = (y[..., 0:2] * 2.0 - 0.5 + self.grid[i]) * self.stride[i]  # xy
+                    wh = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i].view(
+                        1, self.na, 1, 1, 2
+                    )  # wh
                     y = torch.cat((xy, wh, y[..., 4:]), -1)
                 z.append(y.view(bs, -1, self.no))
 
@@ -63,24 +76,29 @@ def fill_fc_weights(layers):
 
 
 class Model(nn.Module):
-    def __init__(self, config='config/yolov5s.yaml', ch=3, nc=None, anchors=None):  # model, input channels, number of classes
+    def __init__(
+        self, config="config/yolov5s.yaml", ch=3, nc=None, anchors=None
+    ):  # model, input channels, number of classes
         super(Model, self).__init__()
         print(config)
         if isinstance(config, dict):
             self.yaml = config  # model dict
         else:  # is *.yaml
             import yaml  # for torch hub
+
             self.yaml_file = Path(config).name
             with open(config) as f:
                 self.yaml = yaml.safe_load(f)  # model dict
 
         # Define model
-        ch = self.yaml['ch'] = self.yaml.get('ch', ch)  # input channels
-        if nc and nc != self.yaml['nc']:
-            self.yaml['nc'] = nc  # override yaml value
-        self.model, self.save = parse_model(deepcopy(self.yaml), ch=[ch])  # model, savelist
-        self.names = [str(i) for i in range(self.yaml['nc'])]  # default names
-        self.inplace = self.yaml.get('inplace', True)
+        ch = self.yaml["ch"] = self.yaml.get("ch", ch)  # input channels
+        if nc and nc != self.yaml["nc"]:
+            self.yaml["nc"] = nc  # override yaml value
+        self.model, self.save = parse_model(
+            deepcopy(self.yaml), ch=[ch]
+        )  # model, savelist
+        self.names = [str(i) for i in range(self.yaml["nc"])]  # default names
+        self.inplace = self.yaml.get("inplace", True)
         # logger.info([x.shape for x in self.forward(torch.zeros(1, ch, 64, 64))])
 
     def forward(self, x, augment=False, profile=False):
@@ -90,7 +108,11 @@ class Model(nn.Module):
         y, dt = [], []  # outputs
         for m in self.model:
             if m.f != -1:  # if not from previous layer
-                x = y[m.f] if isinstance(m.f, int) else [x if j == -1 else y[j] for j in m.f]  # from earlier layers
+                x = (
+                    y[m.f]
+                    if isinstance(m.f, int)
+                    else [x if j == -1 else y[j] for j in m.f]
+                )  # from earlier layers
 
             x = m(x)  # run
             y.append(x if m.i in self.save else None)  # save output
@@ -99,10 +121,10 @@ class Model(nn.Module):
 
 
 def parse_model(d, ch):  # model_dict, input_channels(3)
-    nc, gd, gw = d['nc'], d['depth_multiple'], d['width_multiple']
+    nc, gd, gw = d["nc"], d["depth_multiple"], d["width_multiple"]
 
     layers, save, c2 = [], [], ch[-1]  # layers, savelist, ch out
-    for i, (f, n, m, args) in enumerate(d['backbone']):  # from, number, module, args
+    for i, (f, n, m, args) in enumerate(d["backbone"]):  # from, number, module, args
         m = eval(m) if isinstance(m, str) else m  # eval strings
         for j, a in enumerate(args):
             try:
@@ -111,7 +133,18 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
                 pass
 
         n = max(round(n * gd), 1) if n > 1 else n  # depth gain
-        if m in [Conv, Bottleneck, SPP, DWConv, Focus, BottleneckCSP, C3, C3TR, DeConv, DeConvDCN]:
+        if m in [
+            Conv,
+            Bottleneck,
+            SPP,
+            DWConv,
+            Focus,
+            BottleneckCSP,
+            C3,
+            C3TR,
+            DeConv,
+            DeConvDCN,
+        ]:
             c1, c2 = ch[f], args[0]
             c2 = make_divisible(c2 * gw, 8)
 
@@ -130,11 +163,20 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
         else:
             c2 = ch[f]
 
-        m_ = nn.Sequential(*[m(*args) for _ in range(n)]) if n > 1 else m(*args)  # module
-        t = str(m)[8:-2].replace('__main__.', '')  # module type
+        m_ = (
+            nn.Sequential(*[m(*args) for _ in range(n)]) if n > 1 else m(*args)
+        )  # module
+        t = str(m)[8:-2].replace("__main__.", "")  # module type
         np = sum([x.numel() for x in m_.parameters()])  # number params
-        m_.i, m_.f, m_.type, m_.np = i, f, t, np  # attach index, 'from' index, type, number params
-        save.extend(x % i for x in ([f] if isinstance(f, int) else f) if x != -1)  # append to savelist
+        m_.i, m_.f, m_.type, m_.np = (
+            i,
+            f,
+            t,
+            np,
+        )  # attach index, 'from' index, type, number params
+        save.extend(
+            x % i for x in ([f] if isinstance(f, int) else f) if x != -1
+        )  # append to savelist
         layers.append(m_)
         if i == 0:
             ch = []
@@ -152,9 +194,10 @@ class PoseYOLOv5s(nn.Module):
             fc = nn.Sequential(
                 nn.Conv2d(64, 64, kernel_size=3, padding=1, bias=True),
                 nn.SiLU(),
-                nn.Conv2d(64, num_output, kernel_size=1, stride=1, padding=0))
+                nn.Conv2d(64, num_output, kernel_size=1, stride=1, padding=0),
+            )
             self.__setattr__(head, fc)
-            if 'hm' in head:
+            if "hm" in head:
                 fc[-1].bias.data.fill_(-2.19)
             else:
                 fill_fc_weights(fc)
@@ -169,13 +212,9 @@ class PoseYOLOv5s(nn.Module):
 
 def get_pose_net(num_layers, heads, head_conv):
     config_file = os.path.join(
-        os.path.dirname(__file__),
-        'networks/config/yolov5s.yaml'
+        os.path.dirname(__file__), "networks/config/yolov5s.yaml"
     )
-    pretrained = os.path.join(
-        os.path.dirname(__file__),
-        '../../../models/yolov5s.pt'
-    )
+    pretrained = os.path.join(os.path.dirname(__file__), "../../../models/yolov5s.pt")
     model = PoseYOLOv5s(heads, config_file)
     initialize_weights(model, pretrained)
     return model
@@ -183,10 +222,14 @@ def get_pose_net(num_layers, heads, head_conv):
 
 def intersect_dicts(da, db, exclude=()):
     # Dictionary intersection of matching keys and shapes, omitting 'exclude' keys, using da values
-    return {k: v for k, v in da.items() if k in db and not any(x in k for x in exclude) and v.shape == db[k].shape}
+    return {
+        k: v
+        for k, v in da.items()
+        if k in db and not any(x in k for x in exclude) and v.shape == db[k].shape
+    }
 
 
-def initialize_weights(model, pretrained=''):
+def initialize_weights(model, pretrained=""):
     for i, m in enumerate(model.modules()):
         t = type(m)
         if t is nn.Conv2d:
@@ -202,7 +245,7 @@ def initialize_weights(model, pretrained=''):
         for i, m in enumerate(final_layer.modules()):
             if isinstance(m, nn.Conv2d):
                 if m.weight.shape[0] == model.heads[head]:
-                    if 'hm' in head:
+                    if "hm" in head:
                         nn.init.constant_(m.bias, -2.19)
                     else:
                         nn.init.normal_(m.weight, std=0.001)
@@ -210,10 +253,15 @@ def initialize_weights(model, pretrained=''):
 
     if os.path.isfile(pretrained):
         ckpt = torch.load(pretrained)  # load checkpoint
-        state_dict = ckpt['model'].float().state_dict()  # to FP32
-        state_dict = intersect_dicts(state_dict, model.backbone.state_dict())  # intersect
+        state_dict = ckpt["model"].float().state_dict()  # to FP32
+        state_dict = intersect_dicts(
+            state_dict, model.backbone.state_dict()
+        )  # intersect
         model.backbone.load_state_dict(state_dict, strict=False)  # load
-        print('Transferred %g/%g items from %s' % (len(state_dict), len(model.state_dict()), pretrained))  # report
+        print(
+            "Transferred %g/%g items from %s"
+            % (len(state_dict), len(model.state_dict()), pretrained)
+        )  # report
 
 
 def make_divisible(x, divisor):
